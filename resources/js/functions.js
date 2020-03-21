92dec834ff4d9aa7e4658ea2b6d41f749581fa9b
Andy-thor <andy.dev536@gmail.com>
2020-03-20 23:59:39 -0300

    Download button updated and added option to download on Windows systems.

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -13,7 +13,27 @@ function generateTextCopyright() {
 	$("#owner").html("Copyright &copy; " + strTimeLapse + "<br>" + developerFullName);
 }
 
-function collapsibleButton () {
+function generateButtonDownload() {
+	var nameFile = "";
+	var sizeFile = "";
+	var currentOS = getOS();
+	var urlDownload = "https://codeload.github.com/Andy-thor/StickMan/zip/master";
+	if (currentOS === "Windows") {
+		nameFile = "stickman-0.3.1.exe";
+		sizeFile = "1.9 MB";
+		urlDownload = "https://github.com/Andy-thor/StickMan/releases/download/v0.3.1/stickman-0.3.1.exe";
+	} else {
+		nameFile = "StickMan-master.zip";
+		sizeFile = "483 kB";
+	}
+	$("#details-download-file").html(
+		"<p><span id='name-file' class='bold-text'>Name file:</span> " + nameFile + "</p>" +
+		"<p><span id='size-file' class='bold-text size-bytes'>Size:</span> " + sizeFile + "</p>" +
+		"<p><span id='platform' class='bold-text'>OS supported:</span> " + currentOS + "</p>");
+	$("a.button-download").html("<a href='" + urlDownload + "'>Download StickMan V0.3.1 para Linux</a>");
+}
+
+function collapsibleButton() {
 	var coll = document.getElementsByClassName("collapsible");
 	var i;
 
@@ -33,6 +53,7 @@ function collapsibleButton () {
 function configureEffects() {
 	showSlidesAutomatically();
 	collapsibleButton();
+	generateButtonDownload();
 }
 
 function init() {
